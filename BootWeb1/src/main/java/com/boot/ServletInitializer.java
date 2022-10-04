92aee5ae889eeee40a69820d2b6cbf77a1a094f3
wonghye <dearhw117@naver.com>
2022-10-04 18:14:45 +0900

    1004

--- BootWeb1/src/main/java/com/boot/ServletInitializer.java
+++ BootWeb1/src/main/java/com/boot/ServletInitializer.java
@@ -2,7 +2,7 @@ package com.boot;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+//
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
