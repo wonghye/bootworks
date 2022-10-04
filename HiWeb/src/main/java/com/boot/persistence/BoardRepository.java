d16c2bac1394c5a549371cb36b9e47c52dee252c
wonghye <dearhw117@naver.com>
2022-10-04 18:17:47 +0900

    1004

--- HiWeb/src/main/java/com/boot/persistence/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boot.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.boot.domain.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Long>{
-
-}
